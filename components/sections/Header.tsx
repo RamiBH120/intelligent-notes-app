c57a9f3631ec4d65ccc92e7189ee331d84957379
Rami Ben Hamouda <87983356+RamiBH120@users.noreply.github.com>
2025-12-30 21:40:22 +0100

    fixed header logo overlap

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -16,11 +16,11 @@ const Header = async () => {
     <header className="relative flex h-24 w-full px-3 sm:px-8 items-center justify-between bg-popover shadow-sm">
 
       {user && <SidebarTrigger variant={"default"} size={"icon-lg"} className=" absolute left-1 top-0 bottom-0 my-auto active:bg-muted" />}
-      <Link href={user ? "/" : "/home"} className="flex items-center gap-2 px-2.5 md:px-3 mx-1 md:mx-2 my-auto w-2xl ml-3 text-indigo-500 hover:text-indigo-400 transition-all duration-150" >
+      <Link href={user ? "/" : "/home"} className="flex items-center gap-2 px-3 md:px-4 mx-1 md:mx-2 my-auto w-2xl ml-2 md:ml-3 text-indigo-500 hover:text-indigo-400 transition-all duration-150" >
         <Image src={"/sticky-note.png"} alt="App Logo" width={40} height={40} className="rounded-md" />
         <h1 className="flex flex-col md:text-2xl font-semibold">{appName.split(" ")[0]} <span className="block">{appName.split(" ").slice(1).join(" ")}</span></h1>
       </Link>
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-2 md:gap-4 items-center">
         <span className="hidden md:flex gap-4">
           <Link href="https://rami-benhamouda.tech/#about" target="_blank" className="hover:underline inline-flex items-center mx-1 gap-1"><InfoIcon size={16} /> <span>About</span></Link>
           <Link href="https://rami-benhamouda.tech/#contact" target="_blank" className="hover:underline inline-flex items-center mx-1 gap-1"> <PhoneCallIcon size={16} /> <span>Contact</span></Link>
